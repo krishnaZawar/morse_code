738e30c8cb11d6d6d33cc2b57e0975b9e8f666b9
krishna zawar <zawarkrishna17@gmail.com>
2024-03-06 16:22:14 +0530

    codes list modified, morse code can be decoded

--- morse_code.py
+++ morse_code.py
@@ -1,17 +1,36 @@
-from codes_list import codes
+from codes_list import codes, keys
 
 def generateMorseCode(sentence: str) -> str:
     '''
-    function returns a morse code generated for the given argument
+    function returns a morse code generated for the given sentence
     '''
 
     morse_code = ""
 
     for letter in sentence:
         if letter >= 'a' and letter <= 'z':
-            morse_code += codes[letter.upper()]
+            morse_code += codes[keys.index(letter.upper())]
         else:
-            morse_code += codes[letter]
+            morse_code += codes[keys.index(letter)]
         morse_code += " "
     
-    return morse_code
+    return morse_code.strip()
+
+def decodeMessage(encoded_msg: str) -> str:
+    '''
+    function decodes a given morse code
+    '''
+    msg = ""
+    individual_code = ""
+    for code_bit in encoded_msg:
+        if code_bit == ' ':
+            if individual_code:
+                msg += keys[codes.index(individual_code)]
+                individual_code = ""
+            continue
+        else:
+            individual_code += code_bit
+    msg += keys[codes.index(individual_code)]
+    return msg
+
+print(decodeMessage(".... . .-.. .-.. --- / .-- --- .-. .-.. -.."))
